56664f5b2fcfe81bd127fae569dae33974c4581b
EC2 Default User <ec2-user@ip-172-16-159-238.ec2.internal>
2022-10-18 18:33:38 +0000

    fixed issue with flow logs and parquet

--- jupyter/jupyter_athena.py
+++ jupyter/jupyter_athena.py
@@ -44,20 +44,16 @@ def run_query(sql):
     df = as_pandas(cursor)
     return df
 
-def get_vpc_flow_by_eni(eni_id):
+def get_vpc_flow_by_account(account_id):
     today = date.today()
     yesterday = today - timedelta(days = 1)
     
     sql = f"""SELECT interface_id, srcaddr, srcport, dstaddr, dstport, count(packets) flow_count, sum(packets) packet_count, sum(bytes) sum_bytes
 FROM "{database_name}"."vpc_flow_logs" 
-WHERE ((year = '{str(today.year)}'
-AND month ='{str(today.month).zfill(2)}'
-AND day >= '{str(today.day).zfill(2)}') OR (
-year = '{str(yesterday.year)}'
-AND month ='{str(yesterday.month).zfill(2)}'
-AND day >= '{str(yesterday.day).zfill(2)}'))
-AND interface_id = '{eni_id}'
+WHERE "timestamp" >= '{str(yesterday.year).zfill(2)}/{str(yesterday.month).zfill(2)}/{str(yesterday.day).zfill(2)}'
+AND accountid = '{account_id}'
 GROUP BY interface_id, srcaddr, srcport, dstaddr, dstport
 ORDER BY count(packets) DESC
 limit 100;"""
+
     return run_query(sql)
