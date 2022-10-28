919cabc4379a81cc6001e1ca9246de46d433890a
EC2 Default User <ec2-user@ip-172-16-142-98.ec2.internal>
2022-10-28 00:41:36 +0000

    First IR Runbook

--- jupyter/jupyter-ir-tools/jupyterirtools/athena.py
+++ jupyter/jupyter-ir-tools/jupyterirtools/athena.py
@@ -3,12 +3,17 @@ from datetime import timedelta
 import boto3
 from IPython import display
 import json
+import os
 import pyathena as pa
 import pandas as pd
 from pyathena.pandas.util import as_pandas
 from pyathena import connect
 
-session = boto3.Session(profile_name='default')
+from . import sso
+
+
+
+session = sso.get_session("Jupyter-IR-AdministratorAccess", os.environ['MANAGEMENT_ACCOUNT'])
 ssm_client = session.client('ssm')
     
 management_account_response = ssm_client.get_parameter(Name='Jupyter-Management-Account')
@@ -36,10 +41,26 @@ def run_query(sql):
     print(f"profile_name={profile_name}")
     cursor = connect(s3_staging_dir=s3_staging_dir, region_name=management_region, profile_name=profile_name).cursor()
 
+    sql = sql.replace('${database_name}', database_name)
+    
     cursor.execute(sql)
 
     df = as_pandas(cursor)
     return df
+
+def get_vpc_flow_by_account_eni(account_id, eni):
+    today = date.today()
+    yesterday = today - timedelta(days = 1)
+    
+    sql = f"""SELECT interface_id, srcaddr, srcport, dstaddr, dstport, count(packets) flow_count, sum(packets) packet_count, sum(bytes) sum_bytes
+FROM "{database_name}"."vpc_flow_logs" 
+WHERE "timestamp" >= '{str(yesterday.year).zfill(2)}/{str(yesterday.month).zfill(2)}/{str(yesterday.day).zfill(2)}'
+AND accountid = '{account_id}' AND interface_id = '{eni}'
+GROUP BY interface_id, srcaddr, srcport, dstaddr, dstport
+ORDER BY count(packets) DESC
+limit 100;"""
+
+    return run_query(sql)
 
 def get_vpc_flow_by_account(account_id):
     today = date.today()
