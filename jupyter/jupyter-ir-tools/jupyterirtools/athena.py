958112ba72396b64e00930dccf3ba82496fd0cfd
Mike Gillespie <gillemi@amazon.com>
2023-04-19 13:35:51 +0000

    Cleaned up unneeded code

--- jupyter/jupyter-ir-tools/jupyterirtools/athena.py
+++ jupyter/jupyter-ir-tools/jupyterirtools/athena.py
@@ -1,62 +1,56 @@
 from datetime import date
-import time
 from datetime import timedelta
-import datetime
+from datetime import datetime 
+import time
 import boto3
-from IPython import display
 import json
 import os
-import pyathena as pa
 import pandas as pd
-from pyathena.pandas.util import as_pandas
-from pyathena import connect
-import datetime 
-import time
-    
-from . import sso
 
+QUERY_TIMEOUT = int( os.environ.get('QUERY_TIMEOUT', '120'))
+CATALOG = os.environ.get('CATALOG', "AwsDataCatalog")
 
-
-session = sso.get_session("Jupyter-IR-AdministratorAccess", os.environ['LOGGING_ACCOUNT'])
-ssm_client = session.client('ssm')
-    
-
-database_response = ssm_client.get_parameter(Name='Jupyter-Athena-Glue-Database')
-cloudtrail_response = ssm_client.get_parameter(Name='Jupyter-Athena-CloudTrail-Table')
-flowlogs_response = ssm_client.get_parameter(Name='Jupyter-Athena-FlowLogs-Table')
-    
-management_account = os.environ['MANAGEMENT_ACCOUNT']
-management_region = 'us-east-1'
-logging_account = "913149361159" #os.environ['LOGGING_ACCOUNT']
-cloudtrail_table = cloudtrail_response['Parameter']['Value']
-flowlogs_table = flowlogs_response['Parameter']['Value']
-database_name = database_response['Parameter']['Value']
-
-
+named_queries = None
 
 def run_named_query(source, queryname, params=[]):
-    athena_client = boto3.client('athena')
+    global named_queries
+    session = boto3.session.Session()
+    
+    athena_client = session.client('athena')
+    
+    if named_queries is None:
+        paginator = athena_client.get_paginator('list_named_queries')
 
-    response1 = athena_client.list_named_queries()#NamedQueryId="cloudtrail_security_lake")
+        named_queries = {}
 
-    response = athena_client.batch_get_named_query(
-        NamedQueryIds=response1['NamedQueryIds']
-    )
+        for page in paginator.paginate(PaginationConfig={'PageSize': 50, 'MaxItems': 200}):
+            
+            response = athena_client.batch_get_named_query(
+                NamedQueryIds=page['NamedQueryIds']
+            )
 
-    named_queries = {}
-    for named_query in response['NamedQueries']:
-        named_queries[named_query['Name']] = named_query   
+            for named_query in response['NamedQueries']:
+                named_queries[named_query['Name']] = named_query   
         
-    sql = named_queries[f"{queryname}_{source}"]['QueryString']
+    named_query = named_queries[f"{queryname}_{source}"]
     
-    s3_staging_dir="s3://aws-athena-query-results-{}-{}/".format(logging_account, management_region)
+    return run_query_direct(named_query['QueryString'], named_query['Database'], params)
 
+def run_query_direct(query_string, database, params = []):
+    session = boto3.session.Session()
+    athena_client = session.client('athena')
+    sts_client = session.client('sts')
+    account_id = sts_client.get_caller_identity()["Account"]
     
+    s3_staging_dir="s3://aws-athena-query-results-{}-{}/".format(account_id, session.region_name)
+    
+    timeout_seconds = QUERY_TIMEOUT
+
     response = athena_client.start_query_execution(
-        QueryString=sql,
+        QueryString=query_string,
         QueryExecutionContext={
-            "Database": "amazon_security_lake_glue_db_us_east_1",
-            "Catalog": "AwsDataCatalog"
+            "Database": database,
+            "Catalog": CATALOG
         },
         ResultConfiguration={
             'OutputLocation': s3_staging_dir,
@@ -67,15 +61,8 @@ def run_named_query(source, queryname, params=[]):
         ExecutionParameters=params
     )
     query_execution_id = response['QueryExecutionId']
-    return run_query_direct(query_execution_id, params)
-
-def run_query_direct(query_execution_id, params = []):
-    timeout_seconds = 15
-
-    timeout = datetime.datetime.now() + datetime.timedelta(seconds = timeout_seconds)
-
-
-    athena_client = boto3.client('athena')
+    
+    timeout = datetime.now() + timedelta(seconds = timeout_seconds)
 
     response = athena_client.get_query_execution(
         QueryExecutionId=query_execution_id
@@ -83,7 +70,7 @@ def run_query_direct(query_execution_id, params = []):
 
     status = response.get('QueryExecution', {}).get('Status', {}).get('State', "FAILED") 
 
-    while datetime.datetime.now() < timeout and (status == "RUNNING" or status == "QUEUED"):
+    while datetime.now() < timeout and (status == "RUNNING" or status == "QUEUED"):
         time.sleep(1)
         response = athena_client.get_query_execution(
             QueryExecutionId=query_execution_id
@@ -110,134 +97,3 @@ def run_query_direct(query_execution_id, params = []):
     df = pd.DataFrame.from_dict(results)
     return df
 
-def run_query(sql, params=[]):
-    sts_client = boto3.client('sts')
-
-    response = sts_client.get_caller_identity()
-    region_name = boto3.session.Session().region_name
-    accountId = response['Account']
-
-    s3_staging_dir="s3://aws-athena-query-results-{}-{}/".format(logging_account, management_region)
-
-    profile_name = f"Jupyter-IR-AdministratorAccess-{logging_account}"
-    
-    print(f"profile_name={profile_name}")
-    cursor = connect(s3_staging_dir=s3_staging_dir, region_name=management_region, profile_name=profile_name).cursor()
-
-    sql = sql.replace('${database_name}', database_name)
-    sql = sql.replace('${cloudtrail_table}', cloudtrail_table)
-    
-    cursor.execute(sql)
-
-    df = as_pandas(cursor)
-    return df
-
-def get_vpc_flow_by_account_eni(account_id, eni):
-    today = date.today()
-    yesterday = today - timedelta(days = 1)
-    
-    sql = f"""SELECT interface_id, srcaddr, srcport, dstaddr, dstport, count(packets) flow_count, sum(packets) packet_count, sum(bytes) sum_bytes
-FROM "{database_name}"."{flowlogs_table}" 
-WHERE "timestamp" >= '{str(yesterday.year).zfill(2)}/{str(yesterday.month).zfill(2)}/{str(yesterday.day).zfill(2)}'
-AND accountid = '{account_id}' AND interface_id = '{eni}'
-GROUP BY interface_id, srcaddr, srcport, dstaddr, dstport
-ORDER BY count(packets) DESC
-limit 100;"""
-
-    return run_query(sql)
-
-def get_vpc_flow_by_account(account_id):
-    today = date.today()
-    yesterday = today - timedelta(days = 1)
-    
-    sql = f"""SELECT interface_id, srcaddr, srcport, dstaddr, dstport, count(packets) flow_count, sum(packets) packet_count, sum(bytes) sum_bytes
-FROM "{database_name}"."{flowlogs_table}" 
-WHERE "timestamp" >= '{str(yesterday.year).zfill(2)}/{str(yesterday.month).zfill(2)}/{str(yesterday.day).zfill(2)}'
-AND accountid = '{account_id}'
-GROUP BY interface_id, srcaddr, srcport, dstaddr, dstport
-ORDER BY count(packets) DESC
-limit 100;"""
-
-    return run_query(sql)
-
-def get_vpc_flow_by_account_region(account_id, region_name):
-    session = sso.get_session("Jupyter-IR-AdministratorAccess", account_id, region_name)
-    client = session.client('ec2')
-    response = client.describe_flow_logs()
-    today = datetime.datetime.now()
-    yesterday = today - timedelta(days = 1)
-
-    flow_log_ids = []
-    for flow_log in response['FlowLogs']:
-        flow_log_id = flow_log['FlowLogId']
-        options = "cloudwatch"
-        
-        if "DestinationOptions" in flow_log:
-            options = flow_log['DestinationOptions']['FileFormat']
-        
-        if options == "cloudwatch":
-            log_group_name = flow_log['LogGroupName']
-            return get_from_cloudwatch(account_id, region_name, log_group_name)
-        else:
-            return get_from_athena(account_id, region_name)
-            
-def get_from_athena(account_id, region_name):
-    
-    today = date.today()
-    yesterday = today - timedelta(days = 1)
-    
-    sql = f"""SELECT interface_id, srcaddr, srcport, dstaddr, dstport, count(packets) flow_count, sum(packets) packet_count, sum(bytes) sum_bytes
-FROM "{database_name}"."{flowlogs_table}" 
-WHERE "timestamp" >= '{str(yesterday.year).zfill(2)}/{str(yesterday.month).zfill(2)}/{str(yesterday.day).zfill(2)}'
-AND accountid = '{account_id}'
-AND region = '{region_name}'
-GROUP BY interface_id, srcaddr, srcport, dstaddr, dstport
-ORDER BY count(packets) DESC
-limit 100;"""
-
-    return run_query(sql)
-
-def get_from_cloudwatch(account_id, region_name, log_group_name):
-    session = sso.get_session("Jupyter-IR-AdministratorAccess", account_id, region_name)
-    
-    cloudwatch_client = session.client('logs')
-            
-    t = int(time.time())
-    y = t - 1440
-
-    query = """
-fields accountId, action, logStatus, interfaceId, bytes, packets, protocol, srcAddr, dstAddr, srcPort, dstPort, start, end, version
-"""
-
-    start_query_response = cloudwatch_client.start_query(
-                logGroupName=log_group_name,
-                startTime=y,
-                endTime=t,
-                queryString=query
-            )
-            
-    query_id = start_query_response['queryId']
-
-    response = None
-            
-
-
-    while response == None or response['status'] == 'Running':
-        print('Waiting for query to complete ...')
-        time.sleep(1)
-        response = cloudwatch_client.get_query_results(
-            queryId=query_id
-        )
-            
-    recs =[]
-            
-          
-    for row in response['results']:
-        rec = {}
-        for field in response['results'][0]:
-            if field['field'] != '@ptr':
-                rec[field['field']] = field['value']
-        recs.append(rec)
-
-    dataframe = pd.DataFrame.from_dict(recs, orient="columns")
-    return dataframe
